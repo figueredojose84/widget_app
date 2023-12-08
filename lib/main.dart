fd9f3b0d0161e20f43e863885c5ea7418afb61a2
Figueredojose84 <v-26668714@micorreo.uny.edu>
2023-12-08 11:48:20 -0400

    HomeScreen creado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:widget_app/config/theme/app_theme.dart';
+
+import 'presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +12,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 5).getTheme(),
+      home: const HomeScreen(),
     );
   }
 }
