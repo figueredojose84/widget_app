08dc8225df384a56a266b484d951c7973c517964
Figueredojose84 <v-26668714@micorreo.uny.edu>
2023-12-08 12:16:03 -0400

    homeScreen implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widget_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,45 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Flutter + material3'),
       ),
+      body: const HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final item = appMenuItems[index];
+        return _CustomListTile(item: item);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    super.key,
+    required this.item,
+  });
+
+  final MenuItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ListTile(
+      leading: Icon(item.icon, color: colors.primary),
+      title: Text(item.title),
+      subtitle: Text(item.subTitle),
+      trailing: Icon(Icons.arrow_forward_ios, color: colors.primary),
+      onTap: () {},
     );
   }
 }
